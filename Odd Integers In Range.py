1cf5be6b336f62b9caa86c141e7d9c9e2cc436e4
Shyam3008 <167611597+Shyam3008@users.noreply.github.com>
2024-04-21 13:20:02 +0530

    Odd Integers In Range.py

---
+++ Odd Integers In Range.py
@@ -0,0 +1,12 @@
+The program must accept two integers X and Y and print the odd integers between them. 
+Input Format: The first line denotes the value of X. The second line denotes the value of Y.
+Output Format: The first line contains the odd integers between X and Y separated by a space. 
+Boundary Conditions: -999999 <= X <= 9999999 X < Y <= 9999999 
+Example Input/Output 1: Input: 1 11 Output: 3 5 7 9 
+Example Input/Output 2: Input: 24 30 Output: 25 27 29
+
+x=int(input())
+y=int(input())
+for i in range(x+1,y):
+    if i % 2 != 0:
+        print(i,end=" ")
